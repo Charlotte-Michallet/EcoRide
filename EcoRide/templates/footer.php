64991c0520f1eb5d5141c9344f4119451b693a2a
Charlotte-Michallet <161527468+Charlotte-Michallet@users.noreply.github.com>
2025-07-16 11:53:00 +0200

    style: Improve footer layout for home and contact

--- EcoRide/templates/footer.php
+++ EcoRide/templates/footer.php
@@ -1,35 +1,27 @@
 </main>
 <footer class="bg-gray-100">
   <div class="relative mx-auto max-w-screen-xl px-4 py-2 sm:px-6 lg:px-8 lg:pt-5">
+
+    <!-- Scroll to top link -->
     <div class="absolute -end-20 -top-5">
-      <a
-        class="btn inline-block rounded-full p-1 text-white shadow-sm transition sm:p-2 lg:p-4"
-        href="#">
-          <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="size-5"
-          viewBox="0 0 20 20"
-          fill="currentColor" >
-          <path fill-rule="evenodd"
-            d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
-            clip-rule="evenodd" />
+      <a class="btn inline-block rounded-full p-1 text-white shadow-sm transition sm:p-2 lg:p-4" href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" class="size-5" viewBox="0 0 20 20" fill="currentColor" >
+          <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
         </svg>
       </a>
     </div>
 
-    <div class="lg:flex lg:items-end lg:justify-between pb-5 items-center">
+    <!-- logo -->
+    <div class="lg:flex lg:items-center lg:justify-between pb-5 items-center">
+      <div>
+        <img  class="h-20 w-35 object-contain" src="assets/img/logo/logo.png" alt="Logo">
+      </div>
 
-        <div>
-          <img  class="h-20 w-35 object-contain" src="assets/img/logo/logo.png" alt="">
-        </div>
+      <!-- Copyright text -->
+      <p class="my-auto mx-auto max-w-md text-center text-gray-500 lg:text-left">EcoRide &copy; 2025. All rights reserved.</p>
 
-        <p class="my-auto mx-auto max-w-md text-center text-gray-500 lg:text-left">
-         Copyright &copy; 2025. All rights reserved.
-        </p>
-
-        <ul
-        class="flex flex-wrap justify-center gap-6 md:gap-8 lg:mt-0 lg:justify-end lg:gap-12"
-      >
+      <!-- Linkes for pages -->
+      <ul class="flex flex-wrap justify-center gap-6 md:gap-8 lg:mt-0 lg:justify-end lg:gap-12">
         <li>
           <a class="text-gray-700 transition hover:text-gray-700/75" href="http://localhost:8080/index.php?controller=pages&action=legal"> Mentions légales </a>
         </li>
@@ -38,12 +30,8 @@
           <a class="text-gray-700 transition hover:text-gray-700/75" href="http://localhost:8080/index.php?controller=pages&action=contact"> Contact </a>
         </li>
       </ul>
-
-
-
     </div>
   </div>
 </footer>
-
 </body>
 </html>
--- EcoRide/templates/pages/legals.php
+++ EcoRide/templates/pages/legals.php
@@ -1,32 +1,29 @@
 <?php require_once "../templates/header.php"; ?>
 
 <div class="flex flex-col p-5 lg:px-48 lg:py-11">
-    <div class="bg-gray-100 p-5 mb-10">
+    <div class="bg-gray-100 p-5 mb-10 rounded-2xl">
         <h1 class="font-bold text-2xl mb-2">Tailwind Rocks!</h1>
         <p class="my-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius eaque a ipsam aliquid omnis,
             beatae possimus recusandae illum rem. Minima sequi voluptas repudiandae id? Quae, facere quam suscipit sed,
             aperiam sapiente pariatur soluta enim perferendis illum veniam excepturi doloribus ducimus voluptatibus
             numquam officiis expedita culpa hic sequi quasi reprehenderit iste obcaecati nostrum. Consequuntur aliquam,
             assumenda architecto animi veniam dolore dolor?</p>
-        <button class="text-white font-semibold bg-blue-600 hover:bg-blue-800 p-2 my-1 rounded">Read More...</button>
     </div>
-    <div class="bg-gray-100 p-5 mb-10">
+    <div class="bg-gray-100 p-5 mb-10 rounded-2xl">
         <h1 class="font-bold text-2xl mb-2">Tailwind Rocks!</h1>
         <p class="my-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius eaque a ipsam aliquid omnis,
             beatae possimus recusandae illum rem. Minima sequi voluptas repudiandae id? Quae, facere quam suscipit sed,
             aperiam sapiente pariatur soluta enim perferendis illum veniam excepturi doloribus ducimus voluptatibus
             numquam officiis expedita culpa hic sequi quasi reprehenderit iste obcaecati nostrum. Consequuntur aliquam,
             assumenda architecto animi veniam dolore dolor?</p>
-        <button class="text-white font-semibold bg-blue-600 hover:bg-blue-800 p-2 my-1 rounded">Read More...</button>
     </div>
-    <div class="bg-gray-100 p-5 mb-10">
+    <div class="bg-gray-100 p-5 mb-10 rounded-2xl">
         <h1 class="font-bold text-2xl mb-2">Tailwind Rocks!</h1>
         <p class="my-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius eaque a ipsam aliquid omnis,
             beatae possimus recusandae illum rem. Minima sequi voluptas repudiandae id? Quae, facere quam suscipit sed,
             aperiam sapiente pariatur soluta enim perferendis illum veniam excepturi doloribus ducimus voluptatibus
             numquam officiis expedita culpa hic sequi quasi reprehenderit iste obcaecati nostrum. Consequuntur aliquam,
             assumenda architecto animi veniam dolore dolor?</p>
-        <button class="text-white font-semibold bg-blue-600 hover:bg-blue-800 p-2 my-1 rounded">Read More...</button>
     </div>
 </div>
 <?php require_once "../templates/footer.php"; ?>
